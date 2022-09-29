cb7f29753d365bce2e4c75f558550035bf0087ae
Andrew Ongko <andrew.ongko@gmail.com>
2022-09-29 16:13:25 +0700

    Setting theme: colors, typography

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Andrew's Blog"
+export const SITE_DESCRIPTION =
+  "Using software engineering and data science to build cool stuff."
