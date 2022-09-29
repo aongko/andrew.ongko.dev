c44bf62b86ac197837a8287b3bed523b9afd7f38
Andrew Ongko <andrew.ongko@gmail.com>
2022-09-29 15:37:29 +0700

    Added Sitemap Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "astro/config"
-
+import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 })
